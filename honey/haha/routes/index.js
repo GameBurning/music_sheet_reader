15677ad59fd2bbf051b50daa8ea66df271d9ed1a
GameBurning <ntgbn@qq.com>
2016-04-25 21:52:06 +0800

    nothing

---
+++ honey/haha/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'XDDD 你是不是傻' });
+});
+
+module.exports = router;
